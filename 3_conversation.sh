fbbae439ed493ef66f9b5895cc0cc2a2f8136c41
hemanthkaipa <hemanthkaipa@gmail.com>
2025-06-05 23:14:58 +0530

    conversation using variables

---
+++ 3_conversation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HUSBAND="Hemanth"
+WIFE="Loki"
+
+echo "${WIFE} : Hello, Start Ayava?"
+echo "${HUSBAND} : Haa, Inka start avaledu..!"
+echo "${WIFE}: Eppudu start Avuthav?"
+echo "${HUSBAND} : Inko half n hour lo start avutha, emaina teskuravala??"
+echo "${WIFE}: Ne Istam..!"
+ech0 "${HUSBAND} : Nee Istam tagaleya.. Hungup..!"
