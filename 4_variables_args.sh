e36bb3d86c7530dc98a68105f48888d2957528e5
hemanthkaipa <hemanthkaipa@gmail.com>
2025-06-05 23:25:54 +0530

    variables with args

---
+++ 4_variables_args.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HUSBAND=$1
+WIFE=$2
+
+echo "${WIFE} : Hello, Start Ayava?"
+echo "${HUSBAND} : Haa, Inka start avaledu..!"
+echo "${WIFE}: Eppudu start Avuthav?"
+echo "${HUSBAND} : Inko half n hour lo start avutha, emaina teskuravala??"
+echo "${WIFE}: Ne Istam..!"
+echo "${HUSBAND} : Nee Istam tagaleya Hungup"
+echo "$0 : is the 0th index argument?"
