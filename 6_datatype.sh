fc0ce598c47d1261a59898a496332dfbc9b08abc
hemanthkaipa <hemanthkaipa@gmail.com>
2025-06-05 23:52:47 +0530

    datatypes

---
+++ 6_datatype.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# by default shell allocate datatype based on assignment
+
+NUM1=100
+NUM2=200
+
+FLOAT1=14.3
+NUM3=20
+
+NAME=HEMANTH
+LASTNAME=KAIPA
+
+DECIMAL=$((FLOAT1+num3))
+REALNUMS=$((NUM1+NUM2))
+COMPLETENAME=$((NAME+LASTNAME))
+
+
+echo "sum of float and integer = $DECIMAL"
+
+echo "Sum of two real numbers are $REALNUMS"
+
+echo "Full Name : $COMPLETENAME"
+
+
+
+
+
