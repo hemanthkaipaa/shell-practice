720cccfb030ad4d1980b207d13f5d3c1d88df3d4
hemanthkaipa <hemanthkaipa@gmail.com>
2025-06-06 00:26:29 +0530

    arrays

--- 7_arrays.sh
+++ 7_arrays.sh
@@ -5,4 +5,4 @@
 IPL_TEAMS=("RCB" "SRH" 'MI' "DC" "CSK" "LSG" "PBKS" "KKR" "GT" "RR")
 echo "FAV TEAM : ${IPL_TEAMS[0]} "
 echo "home team : ${IPL_TEAMS[1]}"
-echo "All Teams : ${IPL_TEAMS[@]}:
+echo "All Teams : ${IPL_TEAMS[@]}"
