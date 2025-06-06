63f6b2f8250a6ae3fafb92c01abb99b46e1a2156
hemanthkaipa <hemanthkaipa@gmail.com>
2025-06-06 23:32:34 +0530

    special variables updated

--- 8_special_variables.sh
+++ 8_special_variables.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "show all variables passed to the script : $@"
+echo "show number of variables passed to the script : $#"
+echo "Display script file name : $0"
+echo "present working directory : $pwd"
+echo "home directory of the user : $HOME"
+echo "current user running the script : $USER"
+echo "PID of the current script :  $$"
+echo "PID of the last command in background :  $!"
+echo "exit status of the last command : $?"
