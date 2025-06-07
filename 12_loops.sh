e714758bd486925cc5f3d9d46693e707c8cb3d25
hemanthkaipa <hemanthkaipa@gmail.com>
2025-06-07 19:41:44 +0530

    Loops

--- 12_loops.sh
+++ 12_loops.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-LOG=/var/log/shell-practice/log.txt
-mkdir -p $LOG
+LOG_DIR=/var/log/shell-practice/
+SCRIPT_FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$LOG_DIR/$SCRIPT_FILE.log
+
+echo "$SCRIPT_FILE"
+mkdir -p $LOG_DIR
 #Loops
 APPS_TO_INSTALL=(mysql nginx nodejs)
 for i in "${APPS_TO_INSTALL[@]}" ; do
-    echo -e "\e[32m $i"
+    echo -e "\e[32m $i" | tee -a $LOG_FILE
 done
 
